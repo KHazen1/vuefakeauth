986fadf62c2b19097991b03b1a3951ac8453d6a2
Kris <you@example.com>
2022-02-23 11:37:22 -0600

    Menu Login Logic

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,3 +1,14 @@
+<script setup>
+import { useRouter } from "vue-router";
+import useAuth from "../composable/useAuth";
+const { isAuthenticated, logout } = useAuth();
+const router = useRouter();
+const loggingOut = () => {
+  logout();
+  router.push("/");
+};
+</script>
+
 <template>
     <div class="text-green-200 bg-green-900">
         <div class="container flex items-center justify-between mx-auto">
@@ -19,13 +30,30 @@
                             About
                         </li>
                     </router-link>
-                    <router-link to="/login">
+                    <router-link 
+                        v-if="!isAuthenticated"
+                        :to="{ path: '/login', name: 'Login' }"
+                    >
                         <li 
                             class="px-4 py-8 hover:cursor-pointer hover:bg-green-500 hover:text-green-800"
                         >
                             Login
                         </li>
                     </router-link>
+                    <router-link v-else :to="{ name: 'Secret' }">
+                        <li
+                            class="px-4 py-8 hover:cursor-pointer hover:bg-green-500 hover:text-green-800"
+                        >
+                             Secret
+                        </li>
+                    </router-link>
+                    <button @click="loggingOut">
+                        <li
+                            class="px-4 py-8 hover:cursor-pointer hover:bg-green-500 hover:text-green-800"
+                        >
+                            Logout
+                        </li>
+                    </button>
                 </ul>
             </nav>
         </div>
