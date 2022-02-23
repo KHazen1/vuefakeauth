c8a36b42bfa543e0c6ba4b1531b66481bb70f1c3
KHazen1 <98141017+KHazen1@users.noreply.github.com>
2022-02-23 06:49:19 -0600

    Initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="bg-blue-900 text-blue-200">
+        <div class="container mx-auto flex items-center justify-between">
+            <h1 class="tracking-tighter text-3xl font-thin">Basic<span class="font-normal">Router</span>
+                </h1>
+            <nav>
+                <ul class="flex space-x-4">
+                    <router-link to="/">
+                        <li 
+                            class="
+                                py-8 px-4 
+                                hover:cursor-pointer hover:bg-blue-500 hover:text-blue-800
+                            "
+                        >
+                            Home
+                        </li>
+                    </router-link>
+                    <router-link to="/about">
+                        <li
+                            class="
+                                py-8 px-4
+                                hover:cursor-pointer hover:bg-blue-500 hover:text-blue-800
+                            "
+                        >
+                            About
+                        </li>
+                    </router-link>
+                    <router-link to="/contact">
+                        <li 
+                            class="
+                                py-8 px-4 
+                                hover:cursor-pointer hover:bg-blue-500 hover:text-blue-800
+                            "
+                        >
+                            Contact
+                        </li>
+                    </router-link>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</template>
